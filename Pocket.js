797b72af8a18830041b3c9bb821aa28d0c62e3d0
Lucy <73023435+LucyUwI@users.noreply.github.com>
2023-02-17 21:14:41 +0000

    Add files via upload

---
+++ Pocket.js
@@ -0,0 +1,25 @@
+JQuery = document.createElement("script");
+JQuery.setAttribute(
+  "src",
+  "https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"
+);
+document.body.appendChild(JQuery);
+
+setInterval(() => {
+  $(function () {
+    $(".volume-bar").hover(
+      function () {
+        $(".main-genericButton-button").hide();
+        $(".main-connectPicker-button").hide();
+        $("#volume-percentage").show();
+      },
+      function () {
+        $(".main-genericButton-button").show();
+        $(".main-connectPicker-button").show();
+        $("#volume-percentage").hide();
+      }
+    );
+  });
+}, 2000);
+if (navigator.userAgent.indexOf("Win") != -1)
+  document.documentElement.style.setProperty("--display", "block");
