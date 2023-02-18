399e94a57fa509b3293b6038d0742ac1b7441a65
Lucy <73023435+LucyUwI@users.noreply.github.com>
2023-02-18 16:49:21 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,52 +1,15 @@
 #!/bin/sh
+if [[ -f "~/.config/spicetify/Themes/Pocket/*" ]]; then
+    rm -rf ~/.config/spicetify/Themes/Pocket;
+fi
 
-set -e
+if [[ -f "~/.config/spicetify/Extensions/Pocket.js" ]]; then
+    rm -rf ~/.config/spicetify/Extensions/Pocket.js;
+fi
 
-function dots {
-    while true; do
-        printf "."
-        sleep 0.1
-    done
-}
-
-echo "Downloading"
-# Setup directories to download to
-spice_dir="$(dirname "$(spicetify -c)")"
-theme_dir="${spice_dir}/Themes"
-ext_dir="${spice_dir}/Extensions"
-
-# Make directories if needed
-mkdir -p "${theme_dir}/Pocket"
-mkdir -p "${ext_dir}"
-
-# Download latest tagged files into correct directories
-theme_url="https://raw.githubusercontent.com/LucyUwI/Pocket/main/Pocket"
-
-# Call dots function in background
-dots &
-dots_pid=$!
-# Avoid kill message for dots
-disown
-
-# Store PIDs of curls to kill later
-pids=()
-curl --silent --output "${theme_dir}/Pocket/color.ini" "${theme_url}/Pocket-Spotify/color.ini" &
-pids+=($!)
-curl --silent --output "${theme_dir}/Pocket/user.css" "${theme_url}/Pocket-Spotify/user.css" &
-pids+=($!)
-curl --silent --output "${ext_dir}/Pocket.js" "${theme_url}/Pocket.js" &
-pids+=($!)
-
-# Wait for all curls to finish and kill dots
-for pid in "${pids[@]}"; do
-    wait $pid
-done
-kill $dots_pid
-echo " Done"
-
-# Apply theme
-echo "Applying theme"
+mkdir ~/.config/spicetify/Themes/Pocket
+curl -fsSL https://raw.githubusercontent.com/LucyUwI/Pocket/main/Pocket-Spotify/user.css -o ~/.config/spicetify/Themes/Pocket/user.css
+curl -fsSL https://raw.githubusercontent.com/LucyUwI/Pocket/main/Pocket-Spotify/color.ini -o ~/.config/spicetify/Themes/Pocket/color.ini
+curl -fsSL https://raw.githubusercontent.com/LucyUwI/Pocket/main/Pocket.js -o ~/.config/spicetify/Extensions/Pocket.js
 spicetify config current_theme Pocket color_scheme Pink extensions Pocket.js
 spicetify apply
-
-echo "All done!"
