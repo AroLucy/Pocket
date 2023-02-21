6a270294898e56d9b5be191aac0e5b1b9116b1cf
Lucy <73023435+LucyUwI@users.noreply.github.com>
2023-02-21 21:37:55 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ if [[ -f "~/.config/spicetify/Extensions/Pocket.js" ]]; then
 fi
 
 mkdir ~/.config/spicetify/Themes/Pocket
-curl -fsSL https://raw.githubusercontent.com/LucyUwI/Pocket/main/Pocket-Spotify/user.css -o ~/.config/spicetify/Themes/Pocket/user.css
-curl -fsSL https://raw.githubusercontent.com/LucyUwI/Pocket/main/Pocket-Spotify/color.ini -o ~/.config/spicetify/Themes/Pocket/color.ini
+curl -fsSL https://raw.githubusercontent.com/LucyUwI/Pocket/main/Pocket/user.css -o ~/.config/spicetify/Themes/Pocket/user.css
+curl -fsSL https://raw.githubusercontent.com/LucyUwI/Pocket/main/Pocket/color.ini -o ~/.config/spicetify/Themes/Pocket/color.ini
 curl -fsSL https://raw.githubusercontent.com/LucyUwI/Pocket/main/Pocket.js -o ~/.config/spicetify/Extensions/Pocket.js
 spicetify config current_theme Pocket color_scheme Pink extensions Pocket.js
 spicetify apply
